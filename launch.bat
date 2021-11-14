a5e785b6dee33c0e9ca479d5dd1a57caac8c29c7
Leopc1977 <leophancaowz@gmail.com>
2021-11-14 22:37:00 +0100

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -3,7 +3,8 @@ cd out
 rd /Q /S Ressources
 cd ../out
 mkdir Ressources
-xcopy /E C:\Users\leophancao\Documents\AnimusDemoCpp\src\Ressources C:\Users\leophancao\Documents\AnimusDemoCpp\out\Ressources
+rem xcopy /E C:\Users\leophancao\Documents\AnimusDemoCpp\src\Ressources C:\Users\leophancao\Documents\AnimusDemoCpp\out\Ressources
+xcopy /E D:\repos\AnimusDemoCpp\src\Ressources D:\repos\AnimusDemoCpp\out\Ressources
 
 rem COMPILE
 g++ src/main.cpp -Iinclude -Llib -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -lSDL2_net -lSDL2_mixer  -o out/monprogramme 
